ec5fd6808ca14b49efece722d36e12cccb8dbcdc
Clara <kim963@wisc.edu>
2024-02-24 17:08:05 -0600

    add flask

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def recommendation():
+    if request.method == 'POST':
+        try:
+            user_flavor = request.form['flavor']
+            user_color = request.form['color']
+            print(user_flavor,user_color)
+        except:
+            print("exception")
+        
+    
+    return render_template('a.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

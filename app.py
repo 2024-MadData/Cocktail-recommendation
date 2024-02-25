d3dfb4c2a4bb892d337172128a0a92f85f8447e2
Clara <kim963@wisc.edu>
2024-02-25 00:05:35 -0600

    develop flask

--- app.py
+++ app.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 def recommendation():
     if request.method == 'POST':
         try:
+            user_color = request.form['color']         
             user_flavor = request.form['flavor']
-            user_color = request.form['color']
-            print(user_flavor,user_color)
+            user_spirit = request.form['spirit']
+            print('--------',user_flavor,user_color,user_spirit)
         except:
             print("exception")
         
     
     return render_template('a.html')
 
+@app.route('/cocktail')
+def cocktail_page():
+    return render_template('cocktail_page.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
